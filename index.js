3c845e673f894aaeedd6fb347c9a0d301f85d080
Andre Felipe Araujo <FelipeDevHML>
2023-04-12 19:06:10 -0300

    Adicionado tratamento de erro (esconde card quando API retorna um erro, borda do input fica vermelha e alert indicando que houve um possível erro na digitação), referenciado o card e pais (para reduzir a quantidade getElementById e tornar código mais limpo e legível)

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-document.getElementById("card").style.setProperty("display", "none", "important")
-let input = document.getElementById("pais")
-input.addEventListener("keypress", function(ev) {
-	if (ev.key === "Enter") {
+// document.getElementById("card").style.setProperty("display", "none", "important")
+
+const card = document.getElementById("card")
+const inputPaises = document.getElementById("pais")
+
+card.style.display = "none"
+
+inputPaises.addEventListener("keypress", function(evento) {
+	if (evento.key === "Enter") {
 	  document.getElementById("button-addon2").click();
 	}
  });
- 
 
-function Procurar(){
-	document.getElementById("card").style.setProperty("display", "block", "important")
-	let pais = document.getElementById("pais").value
+ function procurar() {
+	let pais = inputPaises.value
 	let finalURL = `https://restcountries.com/v3.1/name/${pais}?fullText=true`
-	// console.log(finalURL)
 	fetch(finalURL)
-		.then(
-			function(response){
-				return response.json()
-			})
-		.then(function(data){
+		.then(response => {return response.json()})
+		.then(data => {
 				// console.log(data[0])
 				// console.log(data[0].capital[0])
 				// console.log(data[0].continents[0])
 				// console.log(data[0].population)
-				// console.log(data[0].flags.png)
+
+				card.style.display = "block"
+				inputPaises.style.border = "1px solid #007bff"
 
 				let bandeira = document.getElementById("bandeira")
 				let nome = document.getElementById("nome")
@@ -31,10 +32,16 @@ function Procurar(){
 				let continente = document.getElementById("conti")
 
 				nome.innerHTML = pais
-				capital.innerHTML = data[0].capital[0]
+				capital.innerHTML = data[0].capital
 				continente.innerHTML = data[0].continents[0]
 				populacao.innerHTML = (data[0].population+" pessoas")
 				// bandeira.setAttribute("src", data[0].flags.png)
 				bandeira.src = data[0].flags.png
+			})
+			.catch((error) => {
+				// console.log("ERRO!!!: "+error)
+				card.style.display = "none"
+				window.alert("País não encontrado, certifique-se de colocar o nome em inglês")
+				inputPaises.style.border = "1px solid red"
 			})
 }
